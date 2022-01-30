1b834958a274897b4951cdea44867aa03fd7e486
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-30 23:53:30 +0000

    insertion sort implementation

---
+++ src/Algorithms/Sorting/insertionSort.js
@@ -0,0 +1,16 @@
+const insertionSort = function (arr) {
+  let temp = arr.slice();
+  let animations = [];
+  for (let i = 1; i < temp.length; i++) {
+    let j = i;
+    while (j > 0 && temp[j] < temp[j - 1]) {
+      animations.push({ indices: [j, j - 1], color: 'red', swap: false });
+      animations.push({ indices: [j, j - 1], color: '#64ffda', swap: true });
+      [temp[j], temp[j - 1]] = [temp[j - 1], temp[j]];
+      j -= 1;
+    }
+  }
+  return [animations, arr];
+};
+
+export default insertionSort;
