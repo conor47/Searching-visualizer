4bf1848ba6df54834c517be9fcac4fca9860cf53
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-30 23:55:05 +0000

    selection sort implementation

---
+++ src/Algorithms/Sorting/selectionSort.js
@@ -0,0 +1,21 @@
+const selectionSort = function (arr) {
+  let temp = arr.slice();
+  let animations = [];
+  for (let i = 0; i < temp.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < temp.length; j++) {
+      if (temp[j] < temp[min]) {
+        animations.push({ indices: [j, min], color: 'red', swap: false });
+        animations.push({ indices: [j, min], color: '#64ffda', swap: false });
+        min = j;
+      }
+    }
+    if (min !== i) {
+      [temp[i], temp[min]] = [temp[min], temp[i]];
+      animations.push({ indices: [i, min], color: '#64ffda', swap: true });
+    }
+  }
+  return [animations, arr];
+};
+
+export default selectionSort;
