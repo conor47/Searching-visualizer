362f3a984df06b4f2eaea5be591b42ab511eac5e
Conor ladrigan <conorladrigan95@gmail.com>
2022-01-31 00:01:05 +0000

    sorting reducer

---
+++ src/Context/Reducers/sortingReducer.js
@@ -0,0 +1,16 @@
+import { generateArray } from '../utilities/generateArray';
+
+const stateReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_RUNNING':
+      console.log(`running - ${action.payload.isRunning}`);
+      return { ...state, running: action.payload.isRunning };
+    case 'CREATE_ARRAY':
+      let newArray = generateArray(action.payload.size);
+      return { ...state, array: newArray };
+    default:
+      return { ...state };
+  }
+};
+
+export default stateReducer;
